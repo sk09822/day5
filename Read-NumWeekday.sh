bf757df4121f916face1fd4af582f552bafe5e1f
sk09822 <kanadesandipan.virtuoso@gmail.com>
2021-11-20 11:37:04 +0530

    Numweek day problem

---
+++ Read-NumWeekday.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -x
+
+echo "please enter a digit number"
+read digitnum
+
+if [ $digitnum -eq 1 ]
+then
+	echo "sunday"
+
+elif [ $digitnum -eq 2 ]
+then
+	echo "monday"
+
+elif [ $digitnum -eq 3 ]
+then
+	echo "tuesday"
+
+elif [ $digitnum -eq 4 ]
+then
+	echo "Wednesday"
+
+elif [ $digitnum -eq 5 ]
+then
+	echo "thursday"
+
+elif [ $digitnum -eq 6 ]
+then
+	echo "friday"
+
+elif [ $digitnum -eq 7 ]
+then
+	echo "saturday"
+else
+	echo "Invalid Number"
+fi;
